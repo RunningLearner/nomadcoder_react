ae15fbb26eda85f57dc4defb46483422a6699170
RunningLearner <tkacnsdms@naver.com>
2023-03-03 16:21:31 +0900

    feat: url 파라미터 사용하기

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/movie/:id" element={<Detail />} />
     </Routes>
   );
 }
--- my-app/src/components/Movie.js
+++ my-app/src/components/Movie.js
@@ -1,10 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
-const Movie = ({ coverImg, title, summary, genres }) => {
+import { Link } from "react-router-dom";
+
+const Movie = ({ id, coverImg, title, summary, genres }) => {
   return (
     <div>
       <img src={coverImg} alt={title} />
-      <h2>{title}</h2>
+      <h2>
+        <Link to={`/movie/${id}`}>{title}</Link>
+      </h2>
       <p>{summary}</p>
       <ul>
         {genres.map((g) => (
@@ -16,6 +20,7 @@ const Movie = ({ coverImg, title, summary, genres }) => {
 };
 
 Movie.propTypes = {
+  id: PropTypes.number.isRequired,
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
