b73422907fb503d67b86b9e281507dfd2a7bb00c
RunningLearner <tkacnsdms@naver.com>
2023-03-03 09:31:21 +0900

    feat:useeffect 사용해보기

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,21 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => {
+    setCounter((prev) => prev + 1);
+  };
+  console.log("i run alltime");
+  const iRunOnlyOnce = () => {
+    console.log("i run onlyonce");
+  };
+  useEffect(iRunOnlyOnce, []);
   return (
     <div>
-      <h1 className={styles.title}>welcome</h1>
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}> click me </button>
       <Button text={"Continue"} />
     </div>
   );
